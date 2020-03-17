771d45ba01d8f7c1e8f4f46cdbd8406f500a412e
Md.Istiak Alam <61821125+istiak9410@users.noreply.github.com>
2020-03-17 20:35:57 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('admin/contacts', 'ContactController@getIndex');
+Route::get('admin/contacts/data', 'ContactController@getData');
+Route::post('admin/contacts/store', 'ContactController@postStore');
+Route::post('admin/contacts/update', 'ContactController@postUpdate');
+Route::post('admin/contacts/delete', 'ContactController@postDelete');
